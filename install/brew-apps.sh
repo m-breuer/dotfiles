fa7e1f1240a2aac90853f0b70c6fb258ca5ee7d7
Marcel Breuer <breuer.marcel@outlook.com>
2025-04-29 11:48:45 +0200

    Add additional packages to brew-apps.sh and brew-cask.sh for enhanced development environment

--- install/brew-apps.sh
+++ install/brew-apps.sh
@@ -1,7 +1,17 @@
 # Install Packages
 
 apps=(
-
+    devcontainer
+    docker-machine
+    jupyterlab
+    sqlite
+    webp
+    wget
+    curl
+    zsh
+    zsh-autosuggestions
+    zsh-syntax-highlighting
+    git
 )
 
 brew install "${apps[@]}"
